71489140e1777671f955f6aad5c8038bc9f05f30
Santiago RM <150164687+SntgRM@users.noreply.github.com>
2024-10-23 23:47:53 -0500

    publishing the App

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Hero from './components/Hero/Hero';
+
+const App = () => {
+  return (
+    <main className="overflow-x-hidden bg-white text-dark">
+      <Hero />
+    </main>
+  );
+};
+
+export default App;
