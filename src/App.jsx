23f93b57bd7d11741cd7fcf181e50cb46c4bdf79
Santiago RM <150164687+SntgRM@users.noreply.github.com>
2024-10-30 02:02:30 -0500

    implemented 'Programs' section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from './components/Hero/Hero';
+import Programs from './components/Programs/Programs';
 
 const App = () => {
   return (
     <main className="overflow-x-hidden bg-white text-dark">
       <Hero />
+      <Programs />
     </main>
   );
 };
