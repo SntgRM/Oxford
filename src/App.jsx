3f2959db4e1e6dc5fa426836c6d83b4e77fe0735
Santiago RM <150164687+SntgRM@users.noreply.github.com>
2024-10-30 22:41:55 -0500

    implemented 'Banner' section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from './components/Hero/Hero';
 import Programs from './components/Programs/Programs';
+import Banner from './components/Banner/Banner';
 
 const App = () => {
   return (
     <main className="overflow-x-hidden bg-white text-dark">
       <Hero />
       <Programs />
+      <Banner />
     </main>
   );
 };
